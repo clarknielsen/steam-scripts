6ed45691abd6b909982b17e944059f34df5b5e46
Clark <cnielsen@trilogyed.com>
2018-07-26 10:56:47 -0700

    wishlist script complete

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ Run these in your developer console on the Steam website to easily scrape and co
 [badges.js](badges.js) - Use on the badges page to see how much it would cost to buy the remaining cards for sets that are almost finished.
 
 [gems.js](gems.js) - Use on your inventory page to see which card sets are worth the most gems.
+
+[wishlist.js](gems.js) - Use on your wishlist page to automatically remove any games that are no longer for sale or have a "very negative" rating.
--- wishlist.js
+++ wishlist.js
@@ -1,8 +1,20 @@
+// loop over embedded game list
 for (let key in g_rgAppInfo) {
   let game = g_rgAppInfo[key];
   
   // find really bad games or games that are no longer sold
-  if (game.review_desc.indexOf("Negative") > 1 || (game.subs.length === 0 && game.release_date > new Date().getTime())) {
-    console.log(game);
+  if (game.review_desc.match(/^(?:Very|Overwhelmingly) Negative$/) || (game.subs.length === 0 && game.release_date > new Date().getTime())) {
+    // get game id from url property
+    let appid = parseInt(game.capsule.match(/[0-9]+(?=\/header)/)[0]);
+
+    // send post request with IDs
+    fetch("remove/", {
+      method: "POST",
+      credentials: "same-origin", // for cookie
+      body: new URLSearchParams(`appid=${appid}&sessionid=${g_sessionID
+      }`)
+    }).then(function() {
+      console.log(`${game.name} removed`);
+    });
   }
 }
